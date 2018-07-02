9502f3eb0690eee45c3cc1bc2ce994f60da4d630
cruftbunny <cruftbunny@gmail.com>
2018-07-02 19:58:40 -0400

    Added router flashing script

---
+++ ubiquiti-flash.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+if [ "$(id -u)" != "0" ]; then
+	echo -e "Must have root permissions.\nExiting."
+	exit 1
+fi
+
+if [ ! $1 ]; then
+	echo -e "Usage: \nsudo ./ubiquiti-flash.sh [IMAGE URI]"
+	exit 1
+fi
+
+# read in image location
+IMAGE=$1
+
+# set eth0 ip
+ifconfig eth0 192.168.1.1 
+
+# start pinging 192.168.1.20 until we get a response
+	while ! true; do ping -c1 192.168.1.20 > /dev/null && break; done
+
+# once ping is successful, load new image
+tftp <<EOF
+connect 192.168.1.20
+mode binary
+trace
+verbose
+put $IMAGE
+quit
+EOF
+
+echo -e "
+\n\nIMAGE FLASHED
+Waiting for DHCP lease...
+(This will take a few minutes...)\n\n\n"
+
+echo -e "(Now is a good time to text someone you love.)"
+
+# reset DHCP and wait for node to connect
+sudo dhclient eth0
+
+echo -e "\n\nSUCCESS!"
